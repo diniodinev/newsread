c6814dec9d3fd80de701b8b7625acf56a1b1f4d6
Dinyo Ilchev Dinev <dinio.dinev@gmail.com>
2020-02-17 22:41:16 +0200

    Add docs

---
+++ docs/assets/app/media-item.component.ts
@@ -0,0 +1,17 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+@Component({
+  selector: 'mw-media-item',
+  templateUrl: './media-item.component.html',
+  styleUrls: ['./media-item.component.css']
+})
+export class MediaItemComponent {
+
+  @Input() mediaItem;
+  @Output() delete = new EventEmitter();
+
+  onDelete(){
+    console.log("Remove element.");
+    this.delete.emit(this.mediaItem);
+  }
+}
