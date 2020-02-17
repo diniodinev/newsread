8b06c127bf88118b043103258249d344580a6a07
Dinyo Ilchev Dinev <dinio.dinev@gmail.com>
2020-02-17 21:33:06 +0200

    Add new files

---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import  {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import {AppModule} from "./assets/app/app.module";
+
+platformBrowserDynamic().bootstrapModule(AppModule);
